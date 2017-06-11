6ba4226d320a0f4e243d5b64894295432ebf086e
mhumeniuk <kermin220>
2017-06-11 11:31:01 +0200

    Add candy.png. Change to PHASER.AUTO. Add controls for cat

--- src/sprites/Cat.js
+++ src/sprites/Cat.js
@@ -1,14 +1,36 @@
 import Phaser from 'phaser'
 
+const getRandom = (a, b) => {
+ return Math.floor((Math.random() * b) + a);
+}
+
+function tweenRotation(object, game, plus) {    
+  var randomDelay = getRandom(1, 5000);    var amountRotation = plus ? 10 : -10;    
+  object.angle = amountRotation;      
+  var move = game.add.tween(object).to({angle: amountRotation}, 2000, Phaser.Easing.Quadratic.InOut, true,0, -1, true);   }
+
+
+
 export default class extends Phaser.Sprite {
-  constructor ({ game, x, y, asset, cursor }) {
+  constructor ({ game, x, y, asset, cursors }) {
     super(game, x, y, asset)
     this.anchor.setTo(0.5)
-    this.cursor = cursor;
+    this.cursors = cursors;
+    game.physics.enable(this, Phaser.Physics.ARCADE);
   }
 
   update () {
-    this.angle += 50
-
+   
+    this.body.velocity.setTo(0,0)
+    if (this.cursors.left.isDown)
+    {
+       this.body.velocity.x = -500;
+        tweenRotation(this, this.game);
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.body.velocity.x = 500;
+        tweenRotation(this, this.game, true);
+    }  
   }
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,15 +1,17 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
 import Cat from '../sprites/Cat'
-let sky;
+let sky, cursors;
 export default class extends Phaser.State {
   init () {}
   preload () {
     this.game.load.image('sky', '/assets/images/background.png');
     this.game.load.image('cat', '/assets/images/the-hero.png');
+     cursors = game.input.keyboard.createCursorKeys();
   }
 
   create () {
+    this.game.time.advancedTiming = true;
     const bannerText = 'Phaser + ES6 + Webpack'
     let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
     banner.font = 'Bangers'
@@ -22,19 +24,22 @@ export default class extends Phaser.State {
     this.cat = new Cat({
       game: this,
       x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'cat'
+      y: this.world.centerY + 50,
+      asset: 'cat',
+      cursors
     })
 
     this.game.add.existing(this.cat)
+    this.cat.scale.setTo(0.8,0.8);
   }
 
   update() {
-    sky.tilePosition.y += 2
+    sky.tilePosition.y += 8
   }
 
   render () {
     if (__DEV__) {
+      this.game.debug.text(game.time.fps, 2, 14, "#00ff00");
       this.game.debug.spriteInfo(this.cat, 120, 147)
     }
   }
